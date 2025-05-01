f3083797fa56ce5b549cb4ac97d39be4d28bff11
osiz00 <ahmedezealdean0125@gmail.com>
2025-05-01 17:46:51 +0300

    test card

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,6 +1,10 @@
 window.TrelloPowerUp.initialize({
     "card-badges": function (t, opts) {
    t.card('all').then(function(card) {
-    console.log(card)
+    console.log(card);
+    return {
+        text: "<s>hello world</s>"
+    };
    })
+
     }})
